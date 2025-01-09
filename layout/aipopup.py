9c27b5543d4fd09687d815cc7ad01d64b0956e18
someone <nikl.free44@gmail.com>
2025-01-09 14:38:06 +0100

    layout changes

--- layout/aipopup.py
+++ layout/aipopup.py
@@ -20,16 +20,17 @@ _ = gettext.gettext
 class AiPopUp ( wx.Dialog ):
 
     def __init__( self, parent ):
-        wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = _(u"AI DLC Einstellungen"), pos = wx.DefaultPosition, size = wx.Size( 520,220 ), style = wx.DEFAULT_DIALOG_STYLE|wx.STAY_ON_TOP )
+        wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = _(u"AI DLC Einstellungen"), pos = wx.DefaultPosition, size = wx.Size( -1,-1 ), style = wx.DEFAULT_DIALOG_STYLE|wx.STAY_ON_TOP )
 
-        self.SetSizeHints( wx.Size( 520,220 ), wx.DefaultSize )
+        self.SetSizeHints( wx.Size( -1,-1 ), wx.DefaultSize )
 
         bSizer9 = wx.BoxSizer( wx.VERTICAL )
 
-        self.m_staticText13 = wx.StaticText( self, wx.ID_ANY, _(u"Das DLC Benötigt einen OpenAI API Schlüssel, \num Ihre Daten an OpenAI zu senden und sie zuverarbeiten"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText13 = wx.StaticText( self, wx.ID_ANY, _(u"Das DLC Benötigt einen OpenAI API Schlüssel, \num Ihre Daten an OpenAI zu senden und sie zuverarbeiten"), wx.DefaultPosition, wx.DefaultSize, wx.ALIGN_CENTER_HORIZONTAL )
         self.m_staticText13.Wrap( -1 )
 
         self.m_staticText13.SetFont( wx.Font( 14, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, wx.EmptyString ) )
+        self.m_staticText13.SetMinSize( wx.Size( 600,70 ) )
 
         bSizer9.Add( self.m_staticText13, 0, wx.ALL, 5 )
 
@@ -38,7 +39,7 @@ class AiPopUp ( wx.Dialog ):
 
         bSizer12 = wx.BoxSizer( wx.VERTICAL )
 
-        self.m_staticText14 = wx.StaticText( self, wx.ID_ANY, _(u"Persöhnlichkeit der Ai bestimmen:"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText14 = wx.StaticText( self, wx.ID_ANY, _(u"Persöhnlichkeit der Ai bestimmen:"), wx.DefaultPosition, wx.DefaultSize, wx.ALIGN_CENTER_HORIZONTAL )
         self.m_staticText14.Wrap( -1 )
 
         self.m_staticText14.SetFont( wx.Font( 12, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, wx.EmptyString ) )
@@ -47,12 +48,12 @@ class AiPopUp ( wx.Dialog ):
 
         bSizer13 = wx.BoxSizer( wx.HORIZONTAL )
 
-        self.m_staticText15 = wx.StaticText( self, wx.ID_ANY, _(u"Schreibe die antwort"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText15 = wx.StaticText( self, wx.ID_ANY, _(u"Schreibe die Antwort"), wx.DefaultPosition, wx.DefaultSize, 0 )
         self.m_staticText15.Wrap( -1 )
 
         bSizer13.Add( self.m_staticText15, 0, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5 )
 
-        self.ai_personality = wx.TextCtrl( self, wx.ID_ANY, _(u"als wärst du ein Fitness Influenza"), wx.DefaultPosition, wx.DefaultSize, wx.TE_BESTWRAP )
+        self.ai_personality = wx.TextCtrl( self, wx.ID_ANY, _(u"als wärst du ein Fitness Influenzer"), wx.DefaultPosition, wx.DefaultSize, wx.TE_BESTWRAP )
         bSizer13.Add( self.ai_personality, 1, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5 )
 
         self.m_staticText16 = wx.StaticText( self, wx.ID_ANY, _(u"."), wx.DefaultPosition, wx.DefaultSize, 0 )
@@ -69,10 +70,10 @@ class AiPopUp ( wx.Dialog ):
         bSizer10 = wx.BoxSizer( wx.HORIZONTAL )
 
         self.cancel_butt = wx.Button( self, wx.ID_ANY, _(u"Abbrechen"), wx.DefaultPosition, wx.DefaultSize, 0 )
-        bSizer10.Add( self.cancel_butt, 0, wx.ALL, 5 )
+        bSizer10.Add( self.cancel_butt, 0, wx.ALL|wx.ALIGN_BOTTOM, 5 )
 
         self.continue_butt = wx.Button( self, wx.ID_ANY, _(u"Weiter"), wx.DefaultPosition, wx.DefaultSize, 0 )
-        bSizer10.Add( self.continue_butt, 0, wx.ALL, 5 )
+        bSizer10.Add( self.continue_butt, 0, wx.ALL|wx.ALIGN_BOTTOM, 5 )
 
 
         bSizer9.Add( bSizer10, 1, wx.ALIGN_CENTER_HORIZONTAL, 5 )
@@ -80,6 +81,7 @@ class AiPopUp ( wx.Dialog ):
 
         self.SetSizer( bSizer9 )
         self.Layout()
+        bSizer9.Fit( self )
 
         self.Centre( wx.BOTH )
 
--- layout/mainFrame.py
+++ layout/mainFrame.py
@@ -20,24 +20,25 @@ _ = gettext.gettext
 class bmiMainFrame ( wx.Frame ):
 
     def __init__( self, parent ):
-        wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = _(u"BMI Rechner AI-DLC"), pos = wx.DefaultPosition, size = wx.Size( 770,460 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+        wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = _(u"BMI Rechner AI-DLC"), pos = wx.DefaultPosition, size = wx.Size( 1000,500 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
-        self.SetSizeHints( wx.Size( 770,460 ), wx.DefaultSize )
+        self.SetSizeHints( wx.Size( 790,460 ), wx.DefaultSize )
 
         content_box = wx.BoxSizer( wx.VERTICAL )
 
-        self.m_staticText8 = wx.StaticText( self, wx.ID_ANY, _(u"BMI AI Rechner"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText8 = wx.StaticText( self, wx.ID_ANY, _(u"BMI-Rechner"), wx.DefaultPosition, wx.Size( -1,-1 ), wx.ALIGN_CENTER_HORIZONTAL )
         self.m_staticText8.Wrap( -1 )
 
         self.m_staticText8.SetFont( wx.Font( 30, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, wx.EmptyString ) )
-        self.m_staticText8.SetMinSize( wx.Size( 280,55 ) )
+        self.m_staticText8.SetMinSize( wx.Size( 350,55 ) )
 
         content_box.Add( self.m_staticText8, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
 
         self.main_content_area = wx.BoxSizer( wx.HORIZONTAL )
 
+        self.main_content_area.SetMinSize( wx.Size( -1,800 ) )
 
-        content_box.Add( self.main_content_area, 1, wx.EXPAND, 5 )
+        content_box.Add( self.main_content_area, 0, wx.ALIGN_CENTER_HORIZONTAL, 5 )
 
 
         self.SetSizer( content_box )
